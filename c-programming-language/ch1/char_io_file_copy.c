6ecb385105568270a946e27480ce8811f50c7636
Mike Yim <mikeyim@unitetheculture.org>
2023-03-26 11:51:18 -0700

    character io: file copying

---
+++ c-programming-language/ch1/char_io_file_copy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* copy input to ouput; 1st version */
+/*
+main() {
+  int c;
+
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
+}
+*/
+
+/* copy input to output; 2nd version */
+main() {
+  int c;
+
+  printf("EOF: %d", EOF);
+
+  while ((c = getchar()) != EOF)
+    putchar(c);
+}
