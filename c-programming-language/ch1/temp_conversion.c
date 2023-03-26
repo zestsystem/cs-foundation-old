065fb6492b080b0e3267ccae1b442b7cb281b219
Mike Yim <mikeyim@unitetheculture.org>
2023-03-26 10:45:23 -0700

    reverse order temp conversion with for loop

--- c-programming-language/ch1/temp_conversion.c
+++ c-programming-language/ch1/temp_conversion.c
@@ -2,7 +2,7 @@
 
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ..., 300 */
-
+/*
 main() {
   float fahr, celsius;
   int lower, upper, step;
@@ -19,5 +19,14 @@ main() {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
+  }
+}
+*/
+
+main() {
+  int fahr;
+
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
   }
 }
